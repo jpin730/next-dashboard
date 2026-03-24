e64731f07e9a954cade857887739ee3fbc994ec5
Jaime Pineda <jpin730@pm.me>
2026-03-24 17:24:39 -0600

    feat: :sparkles: add CartCounter component for managing product count in cart

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,7 +1,17 @@
+import type { Metadata } from 'next'
+
+import { CartCounter } from '@/app/shopping-cart/CartCounter'
+
+export const metadata: Metadata = {
+  title: 'Shopping Cart Counter',
+  description: 'A simple counter',
+}
+
 export default function CounterPage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Counter Page</h1>
+    <div className="flex size-full flex-col items-center justify-center">
+      <p>Products in cart</p>
+      <CartCounter initialCount={20} />
     </div>
   )
 }
---
+++ app/shopping-cart/CartCounter.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useState } from 'react'
+
+interface Props {
+  initialCount?: number
+}
+
+export const CartCounter = ({ initialCount = 10 }: Readonly<Props>) => {
+  const [count, setCount] = useState(initialCount)
+
+  return (
+    <>
+      <p className="text-9xl">{count}</p>
+      <p className="space-x-2">
+        <button
+          className="w-25 rounded-xl bg-gray-900 p-2 text-white transition-all hover:bg-gray-600"
+          onClick={() => setCount(count - 1)}
+        >
+          -1
+        </button>
+        <button
+          className="w-25 rounded-xl bg-gray-900 p-2 text-white transition-all hover:bg-gray-600"
+          onClick={() => setCount(count + 1)}
+        >
+          +1
+        </button>
+      </p>
+    </>
+  )
+}
