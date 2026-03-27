ec99d44720d9bf52ced166f202ba863939094ed9
Jaime Pineda <jpin730@pm.me>
2026-03-26 18:18:02 -0600

    feat: :sparkles: implement fetching pokemon by ID in service

--- src/pokemons/services/pokemons.ts
+++ src/pokemons/services/pokemons.ts
@@ -1,16 +1,22 @@
+import type { Pokemon } from '../interfaces/Pokemon'
 import type { PokemonApiResource } from '../interfaces/PokemonApiResource'
 import type { PokemonApiResponse } from '../interfaces/PokemonApiResponse'
 import type { SimplePokemon } from '../interfaces/SimplePokemon'
 
-const POKEMON_API_URL = 'https://pokeapi.co/api/v2'
+const BASE_URL = 'https://pokeapi.co/api/v2'
 
-const POKEMON_LIMIT = 151
+const LIMIT = 151
 
-export const getPokemons = async (limit = POKEMON_LIMIT, offset = 0): Promise<SimplePokemon[]> => {
-  const response = await fetch(`${POKEMON_API_URL}/pokemon?limit=${limit}&offset=${offset}`)
+export const getPokemons = async (limit = LIMIT, offset = 0): Promise<SimplePokemon[]> => {
+  const response = await fetch(`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`)
   const data: PokemonApiResponse<PokemonApiResource[]> = await response.json()
   return data.results.map(({ name, url }) => ({
     name,
     id: url.split('/').at(-2) ?? '',
   }))
+}
+
+export const getPokemonById = async (id: string): Promise<Pokemon> => {
+  const response = await fetch(`${BASE_URL}/pokemon/${id}`)
+  return response.json()
 }
