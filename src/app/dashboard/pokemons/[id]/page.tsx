28f6655725c88103d4757bd0f03ab7ab230be820
Jaime Pineda <jpin730@pm.me>
2026-03-26 19:40:49 -0600

    feat: :sparkles: enhance PokemonPage with detailed layout and metadata generation

--- src/app/dashboard/pokemons/[id]/page.tsx
+++ src/app/dashboard/pokemons/[id]/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next'
+
 import type { WithParams } from '@/shared/interfaces/WithParams'
+
+import Image from 'next/image'
+
+import { getPokemonById } from '@/pokemons/services/pokemons'
 
 interface Params {
   id: string
@@ -6,12 +12,107 @@ interface Params {
 
 type Props = WithParams<Params>
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params
+  const pokemon = await getPokemonById(id)
+
+  return {
+    title: `#${pokemon.id} ${pokemon.name}`,
+    description: `Details of the pokemon ${pokemon.name}`,
+  }
+}
+
 export default async function PokemonPage({ params }: Readonly<Props>) {
   const { id } = await params
+  const pokemon = await getPokemonById(id)
+
   return (
-    <main>
-      <h1>Pokemon Page</h1>
-      <p>Pokemon ID: {id}</p>
+    <main className="flex h-full items-center justify-center">
+      <div className="mx-auto w-full max-w-175 rounded-2xl bg-white p-4 text-slate-800 shadow-lg">
+        <div className="w-full">
+          <h1 className="px-2 text-xl font-bold text-slate-700 capitalize">
+            #{pokemon.id} {pokemon.name}
+          </h1>
+          <div className="relative flex flex-col items-center justify-center">
+            <Image
+              fill
+              loading="eager"
+              src={pokemon.sprites.other?.dream_world.front_default ?? ''}
+              alt={`Imagen del pokemon ${pokemon.name}`}
+              className="static! mb-4 h-38! object-contain"
+            />
+          </div>
+        </div>
+
+        <div className="mb-4 grid w-full grid-cols-2 gap-4">
+          <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+            <p className="text-sm text-gray-600">Types</p>
+            <p className="text-navy-700 flex text-center text-base font-medium">
+              {pokemon.types.map((type) => (
+                <span key={type.slot} className="capitalize">
+                  {type.type.name}&nbsp;
+                </span>
+              ))}
+            </p>
+          </div>
+
+          <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+            <p className="text-sm text-gray-600">Peso</p>
+            <p className="text-navy-700 flex text-center text-base font-medium">{pokemon.weight}</p>
+          </div>
+
+          <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+            <p className="text-sm text-gray-600">Regular Sprites</p>
+            <div className="flex justify-center">
+              <Image
+                src={pokemon.sprites.front_default}
+                width={100}
+                height={100}
+                alt={`sprite ${pokemon.name}`}
+              />
+
+              <Image
+                src={pokemon.sprites.back_default}
+                width={100}
+                height={100}
+                alt={`sprite ${pokemon.name}`}
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 drop-shadow-lg">
+            <p className="text-sm text-gray-600">Shiny Sprites</p>
+            <div className="flex justify-center">
+              <Image
+                src={pokemon.sprites.front_shiny}
+                width={100}
+                height={100}
+                alt={`sprite ${pokemon.name}`}
+              />
+
+              <Image
+                src={pokemon.sprites.back_shiny}
+                width={100}
+                height={100}
+                alt={`sprite ${pokemon.name}`}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap justify-center gap-1">
+          {pokemon.moves
+            .toSorted((a, b) => a.move.name.localeCompare(b.move.name))
+            .map((move) => (
+              <p
+                key={move.move.name}
+                className="rounded-full bg-gray-200 px-2 py-1 text-xs text-gray-700 capitalize"
+              >
+                {move.move.name}
+              </p>
+            ))}
+        </div>
+      </div>
     </main>
   )
 }
--- src/pokemons/interfaces/Pokemon.ts
+++ src/pokemons/interfaces/Pokemon.ts
@@ -1,4 +1,35 @@
+import type { PokemonApiResource } from './PokemonApiResource'
+
 export interface Pokemon {
   id: number
   name: string
+  weight: number
+  moves: Move[]
+  sprites: Sprites
+  types: Type[]
+}
+
+export interface Move {
+  move: PokemonApiResource
+}
+
+interface Sprites {
+  back_default: string
+  back_shiny: string
+  front_default: string
+  front_shiny: string
+  other?: Other
+}
+
+interface Other {
+  dream_world: DreamWorld
+}
+
+interface DreamWorld {
+  front_default: string
+}
+
+interface Type {
+  slot: number
+  type: PokemonApiResource
 }
