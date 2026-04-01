3843b66b9b4c6a5782cdc8e9dfbc763452da3c5f
Jaime Pineda <jpin730@pm.me>
2026-04-01 16:30:25 -0600

    feat: :sparkles: wrap children in Suspense for improved loading state in DashboardLayout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import { Sidebar } from '@/ui/components/Sidebar'
 
 interface Props {
@@ -9,7 +11,9 @@ export default function DashboardLayout({ children }: Readonly<Props>) {
     <div className="h-screen w-screen overflow-y-scroll bg-slate-100 text-slate-300 antialiased selection:bg-blue-600 selection:text-white">
       <div className="flex">
         <Sidebar />
-        <div className="w-full p-4 text-slate-900">{children}</div>
+        <div className="w-full p-4 text-slate-900">
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        </div>
       </div>
     </div>
   )
